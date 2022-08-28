cc06757fde81c906bcc34193bc6749faabb0fa4e
aoiwebstudio <aoiwebstudio@gmail.com>
2022-08-28 14:39:49 +0900

    177

--- wp-content/themes/pacificmall/front-page.php
+++ wp-content/themes/pacificmall/front-page.php
@@ -94,12 +94,7 @@
     <p class="section-lead"><?php echo $term_obj->description; ?></p>
     <ul class="news">
       <?php
-      $args = array(
-        'post_type' => 'post',
-        'category_name' => 'news',
-        'posts_per_page' => 3,
-      );
-      $news_posts = new WP_Query($args);
+      $news_posts = get_specific_posts('post', 'category', 'news', 3);
       if ($news_posts->have_posts()) :
         while ($news_posts->have_posts()) : $news_posts->the_post();
       ?>
@@ -117,26 +112,28 @@
       ?>
     </ul>
     <div class="section-buttons">
-      <button type="button" class="button button-ghost" onclick="javascript:location.href = '#';">
-        ニュースリリース一覧を見る
+      <button type="button" class="button button-ghost" onclick="javascript:location.href = '<?php echo esc_url(get_term_link($term_obj)); ?>';">
+        <?php echo $term_obj->name; ?>一覧を見る
       </button>
     </div>
   </div>
 </section>
 <section class="section-contents" id="company">
   <div class="wrapper">
-    <span class="section-title-en">Enterprise Information</span>
-    <h2 class="section-title">企業情報</h2>
-    <p class="section-lead">
-      私たちパシフィックモール開発は、<br />
-      ショッピングモール開発を通じて新たな価値を創造し<br />
-      社会に貢献するグローバルな企業を目指します。
-    </p>
+    <?php 
+    $company_page = get_page_by_path('company');
+    $post = $company_page;
+    setup_postdata($post);
+    ?>
+    <span class="section-title-en">Corporate Information</span>
+    <h2 class="section-title"><?php the_title(); ?></h2>
+    <p class="section-lead"><?php echo get_the_excerpt(); ?></p>
     <div class="section-buttons">
-      <button type="button" class="button button-ghost" onclick="javascript:location.href = '#';">
-        企業情報を見る
+      <button type="button" class="button button-ghost" onclick="javascript:location.href = '<?php echo esc_url(home_url('company')); ?>';">
+      <?php echo the_title(); ?>一覧を見る
       </button>
     </div>
+    <?php wp_reset_postdata(); ?>
   </div>
 </section>
 <?php get_footer(); ?>
